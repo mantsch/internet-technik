5b751b99f771d2cf3faa4c125fca46a97aed453c
Manuel Deutsch <hi@manuel-deutsch.de>
2021-06-21 10:48:30 +0200

    add new js for example & rename js file

--- assets/example.js
+++ assets/example.js
@@ -1,37 +1,9 @@
-// Get todays weekday
-const today = whatDay();
+// Simple for example
+// Array => List of strings
+let friends = ['Leo', 'Tiger', 'Lilly', 'Felix'];
+// index 0, 1, 2, 3
 
-// Call our Todo function
-const myTodo = todo(today);
-
-// Log our todo
-console.log(myTodo);
-
-// todo function based on weekday
-function todo(today) {
-  let weekday = today;
-
-  if (weekday === 'friday' || weekday === 'saturday') {
-    return 'drink beer';
-  } else if (weekday === 'sunday') {
-    return 'chill';
-  } else {
-    return 'work';
-  }
-}
-
-// get the name of today
-function whatDay() {
-  // The getDay() method returns the day of the week (from 0 to 6) for the specified date
-  let currentDate = new Date();
-  let currentWeekday = currentDate.getDay();
-  console.log(currentWeekday);
-
-  // Get the weekday name our array
-  const weeklist = ['sunday', 'monday', 'thuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-  let currentWeekdayString = weeklist[currentWeekday];
-  console.log(currentWeekdayString);
-
-  // return todays string name
-  return currentWeekdayString;
+for (let count = 0; count < friends.length; count++) {
+  console.log('Miau ' + friends[count] + '\n');
+  console.log('Count: ' + count + ' Length: ' + friends.length + '\n');
 }
---
+++ assets/today-todos.js
@@ -0,0 +1,37 @@
+// Get todays weekday
+const today = whatDay();
+
+// Call our Todo function
+let myTodo = todo(today);
+
+// Log our todo
+console.log(myTodo);
+
+// todo function based on weekday
+function todo(today) {
+  let weekday = today;
+
+  if (weekday === 'friday' || weekday === 'saturday') {
+    return 'no drink beer';
+  } else if (weekday === 'sunday') {
+    return 'chill';
+  } else {
+    return 'work';
+  }
+}
+
+// get the name of today
+function whatDay() {
+  // The getDay() method returns the day of the week (from 0 to 6) for the specified date
+  let currentDate = new Date();
+  let currentWeekday = currentDate.getDay();
+  console.log(currentWeekday);
+
+  // Get the weekday name our array
+  const weeklist = ['sunday', 'monday', 'thuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  let currentWeekdayString = weeklist[currentWeekday];
+  console.log(currentWeekdayString);
+
+  // return todays string name
+  return currentWeekdayString;
+}
